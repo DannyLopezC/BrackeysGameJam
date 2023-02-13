d8fa910a95863a150faa517dc3ddf94bc17c8f94
Danny López Cárdenas <danny.lopezc00@gmail.com>
2023-02-13 01:18:17 -0500

    Player movement animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
 
     public Vector2 movement;
 
@@ -16,6 +17,14 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        var transformLocalScale = transform.localScale;
+        transformLocalScale.x = movement.x != 0 ? movement.x : transformLocalScale.x;
+        transform.localScale = transformLocalScale;
     }
 
     private void FixedUpdate()
