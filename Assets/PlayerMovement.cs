95780d22aaf1cc3039dda19ebec21f2d049259bf
Danny López Cárdenas <danny.lopezc00@gmail.com>
2023-02-14 00:39:46 -0500

    things

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,12 +12,11 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector2 movement;
 
-    public Sprite upIdle;
-    public Sprite downIdle;
-    public Sprite rightIdle;
-    public Sprite leftIdle;
-
     public SpriteRenderer spriteRenderer;
+
+    private static readonly int FacingSide = Animator.StringToHash("FacingSide");
+    private static readonly int FacingUp = Animator.StringToHash("FacingUp");
+    private static readonly int FacingDown = Animator.StringToHash("FacingDown");
 
     // Update is called once per frame
     void Update()
@@ -26,22 +25,42 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        if (movement.x != 0) spriteRenderer.sprite = rightIdle;
-        else if (movement.y < 0) spriteRenderer.sprite = downIdle;
-        else if (movement.y < 0) spriteRenderer.sprite = upIdle;
-
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        var transformLocalScale = transform.localScale;
-        transformLocalScale.x = movement.x != 0 ? movement.x : transformLocalScale.x;
-        transform.localScale = transformLocalScale;
+        PlayerRotation();
     }
 
     private void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.deltaTime));
+    }
+
+    private void PlayerRotation()
+    {
+        if (movement.x != 0)
+        {
+            animator.SetBool(FacingSide, true);
+            animator.SetBool(FacingUp, false);
+            animator.SetBool(FacingDown, false);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetBool(FacingDown, true);
+            animator.SetBool(FacingSide, false);
+            animator.SetBool(FacingUp, false);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetBool(FacingUp, true);
+            animator.SetBool(FacingSide, false);
+            animator.SetBool(FacingDown, false);
+        }
+
+        var transformLocalScale = transform.localScale;
+        transformLocalScale.x = movement.x != 0 ? movement.x : transformLocalScale.x;
+        transform.localScale = transformLocalScale;
     }
 }
